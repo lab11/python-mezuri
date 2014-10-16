f82d50c5afd1e2d3b74a78e95f74394993055170
Brad Campbell <bradjc5@gmail.com>
2014-10-15 20:30:22 -0400

    including setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='mezuri',
+      version='0.1',
+      description='Python Interface for Mezuri Database',
+      author='Brad Campbell',
+      author_email='bradjc@umich.edu',
+      py_modules=['mezuri']
+     )
