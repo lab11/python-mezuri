6c4b0221c171c659af0df8ffbe485c09dac94716
Brad Campbell <bradjc5@gmail.com>
2014-10-15 21:01:58 -0400

    added a test

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import datetime
+
+import mezuri
+
+print('Making a connection to Mezuri...')
+mt = mezuri.MezuriTable('http://162.222.181.71/odktables', 'mezuri-10100233')
+
+print("Create the table...")
+schema = {'timestamp': mezuri.ColumnTypes.datetime,
+          'value':     mezuri.ColumnTypes.number}
+mt.create('python-mezuri-test', schema)
+
+data = [{'timestamp': datetime.datetime.now(),
+         'value':     10},
+        {'timestamp': datetime.datetime.now(),
+         'value':     22}]
+
+print('Adding data...')
+mt.add_rows(data)
+
+print('Get the data back...')
+rows = mt.get_rows(100)
+for i,row in enumerate(rows):
+	print('row {}'.format(i))
+	for k,v in row.items():
+		print('  {:<10}: {}'.format(k,v))
